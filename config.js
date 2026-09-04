ae4fb69d94abd0f72482df024c114fdaaff074b7
lanter95-collab <lanter95@gmail.com>
2026-09-04 17:22:36 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// 【個人版專用】這個檔案不會被同步，只放在你個人的網頁空間裡
+// 主程式碼（index.html）永遠共用同一份，這支 config.js 才是區分「個人」跟「公司」的地方
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDuwAcj68H7ntSDZxLCuYiR4anHDDZ-rYA",
+  authDomain: "list-tree.firebaseapp.com",
+  projectId: "list-tree",
+  storageBucket: "list-tree.firebasestorage.app",
+  messagingSenderId: "747810820130",
+  appId: "1:747810820130:web:b16d7d6ada64864c55ba7b",
+  measurementId: "G-JM4X24HZB6"
+};
